863505bc4501735cd1ed0bb75774e7146bd0e770
javierdj22 <40278873+javierdj22@users.noreply.github.com>
2025-09-28 12:46:29 -0500

    Mejora Casos
    agrego mas casos para pruebas

--- GestionProductos.Tests/ProductServiceTests.cs
+++ GestionProductos.Tests/ProductServiceTests.cs
@@ -7,17 +7,51 @@ using System;
 
 public class ProductServiceTests
 {
-    [Fact]
-    public async Task AddProduct_ShouldThrow_WhenPriceIsZero()
+    private ProductService GetService()
     {
         var config = new ConfigurationBuilder().AddInMemoryCollection(
             new Dictionary<string, string> { { "ConnectionStrings:MongoDb", "mongodb://localhost:27017" } }
         ).Build();
 
-        var service = new ProductService(config);
-        var product = new Product { Name = "Test1234", Price = 10, Stock = 15 };
+        return new ProductService(config);
+    }
 
-        await Assert.ThrowsAsync<ArgumentException>(async () =>
-            await service.AddProductAsync(product));
+    [Fact]
+    public async Task AddProduct_PriceIsZero()
+    {
+        var service = GetService();
+        var product = new Product { Name = "TestInvalid", Price = 0, Stock = 10 };
+
+        var exception = await Record.ExceptionAsync(
+            async () => await service.AddProductAsync(product)
+        );
+
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public async Task AddProduct_NameNull()
+    {
+        var service = GetService();
+        var product = new Product { Name = "", Price = 10, Stock = 10 };
+
+        var exception = await Record.ExceptionAsync(
+            async () => await service.AddProductAsync(product)
+        );
+
+        Assert.Null(exception);
+    }
+
+    [Fact]
+    public async Task AddProduct_Ok()
+    {
+        var service = GetService();
+        var product = new Product { Name = "TestValid", Price = 10, Stock = 15 };
+
+        var exception = await Record.ExceptionAsync(
+            async () => await service.AddProductAsync(product)
+        );
+
+        Assert.Null(exception); 
     }
 }
